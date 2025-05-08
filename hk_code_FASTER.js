93a15182343c06f08e01ac38309eb1b7961091d0
Kane Kriz <kanemkriz@gmail.com>
2025-05-08 07:50:02 -0600

    Update hk_code_FASTER.js

--- hk_code_FASTER.js
+++ hk_code_FASTER.js
@@ -1,1 +1,38 @@
+//GEEKSFORGEEKS CODE
+
+
+
+
+
+
+
+
+function totalCost(mask, pos, n, cost, memo) {
+    if (mask === (1 << n) - 1) {
+        return cost[pos][0];
+    }
+
+    if (memo[mask][pos] !== undefined) {
+        return memo[mask][pos];
+    }
+
+    let ans = Number.MAX_VALUE;
+
+    for (let i = 0; i < n; i++) {
+        if ((mask & (1 << i)) === 0) {
+            ans = Math.min(ans, cost[pos][i] + totalCost(mask | (1 << i), i, n, cost, memo));
+        }
+    }
+
+    memo[mask][pos] = ans;
+    return ans;
+}
+
+function tsp_hk(cost) {
+    const n = cost.length;
+    const memo = Array.from({ length: 1 << n }, () => Array(n).fill(undefined));
+    return totalCost(1, 0, n, cost, memo);
+}
+
+module.exports = { tsp_hk };
 
